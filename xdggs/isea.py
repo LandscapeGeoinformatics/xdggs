46ae5a9b454ac2b941860d86666fbf0b6fae4865
dick <dick@debian-geolab>
2024-10-27 20:52:15 +0200

    1. update function name grid_cell_centroids_for_extent with dggrid4py    0.2.8 2. update initial parameters of DGGRIDv7 to default 3. update pyproject.toml with dggrid4py==0.2.8

--- xdggs/isea.py
+++ xdggs/isea.py
@@ -161,8 +161,8 @@ class ISEAIndex(DGGSIndex):
                     truck_df = gpd.GeoDataFrame([0], geometry=[box(minlng, minlat, maxlng, maxlat)], crs='wgs84')
                     del y_trunk
                     working_dir = tempfile.mkdtemp()
-                    dggs = DGGRIDv7(executable=executable, working_dir=working_dir, capture_logs=True, silent=True, tmp_geo_out_legacy=False)
-                    result = dggs.grid_centerpoint_for_extent(grid.upper(), resolution, clip_geom=truck_df.geometry.values[0])
+                    dggs = DGGRIDv7(executable=executable, working_dir=working_dir)
+                    result = dggs.grid_cell_centroids_for_extent(grid.upper(), resolution, clip_geom=truck_df.geometry.values[0])
                     centroids = result.geometry.get_coordinates()
                     centroids = np.stack([centroids['x'].values, centroids['y'].values], axis=-1)
                     centroids_idx = S2PointIndex(centroids)
@@ -186,7 +186,7 @@ class ISEAIndex(DGGSIndex):
                     end = (i * step) + step if (((i * step) + step) < x.shape[0]) else x.shape[0]
                     offset = i * (y.shape[0] * step)
                     working_dir = tempfile.mkdtemp()
-                    dggs = DGGRIDv7(executable=executable, working_dir=working_dir, capture_logs=True, silent=True, tmp_geo_out_legacy=False)
+                    dggs = DGGRIDv7(executable=executable, working_dir=working_dir)
                     y_end = (j * step) + step if (((j * step) + step) < y.shape[0]) else y.shape[0]
                     x_trunk, y_trunk = np.broadcast_arrays(x[(i * step):end], y[(j * step):y_end, None])
                     x_trunk = np.stack([x_trunk, y_trunk], axis=-1)
